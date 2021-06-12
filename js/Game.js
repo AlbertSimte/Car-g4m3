a9090182077063abebe14dff306fe70ac3505960
AlbertSimte <74811787+AlbertSimte@users.noreply.github.com>
2021-06-12 22:52:32 +0600

    Add files via upload

---
+++ js/Game.js
@@ -0,0 +1,53 @@
+class Game {
+  constructor(){}
+  
+  getState(){
+    var gameStateRef  = database.ref('gameState');
+    gameStateRef.on("value",function(data){
+       gameState = data.val();
+    })
+   
+  }
+
+  update(state){
+    database.ref('/').update({
+      gameState: state
+    });
+  }
+
+  async start(){
+    if(gameState === 0){
+      player = new Player();
+    var playerCountRef = await  database.ref('playerCount').once("value")
+    if(playerCountRef.exists()){
+      playerCount = playerCountRef.val()
+      player.getCount();
+    }
+      
+      form = new Form()
+      form.display();
+    }
+  }
+
+  play(){
+    form.hide()
+    textSize(30)
+    text("Game Has Started",120,100)
+    Player.getPlayerinfo()
+    console.log(allPlayers)
+    if(allPlayers !== undefined){
+      var displayPosition = 130
+      for(var plr in allPlayers){
+        if(plr === "player" + player.index){
+          fill("red")
+        }
+        else{
+          fill("black")
+        }
+        displayPosition = displayPosition + 20
+        textSize(15)
+        text(allPlayers[plr].name+" " + allPlayers[plr].distance,120,100)
+        }
+    }
+  }
+}
